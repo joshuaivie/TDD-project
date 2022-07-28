e5f5109ed0370ff4a60cddb0c2ac11e97dffe337
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-07-28 20:00:21 +0100

    add check for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,17 +1,26 @@
-require_relative '../solver.rb'
+require_relative '../solver'
 
 describe Solver do
-  context "check for factorial method" do
-    before :each do
-      @solver=Solver.new
-    end
-    it "check for valid result" do
+  before :each do
+    @solver = Solver.new
+  end
+
+  context 'check for factorial method' do
+    it 'check for valid result' do
       expect(@solver.factorial(5)).to eq(120)
       expect(@solver.factorial(4)).to eq(24)
       expect(@solver.factorial(0)).to eq(1)
     end
-    it "check for Exception" do
-      expect(@solver.factorial(-5)).to raise_error("Integer must be a posetive whole number")
+    it 'check for Exception' do
+      expect(@solver.factorial(-5)).to raise_error('Integer must be a posetive whole number')
+    end
+  end
+
+  context 'check reverse method' do
+    it 'reverses the inputted string' do
+      expect(@solver.reverse('hello')).to eq('hello')
+      expect(@solver.reverse('red')).to eq('der')
+      expect(@solver.reverse('chair')).to eq('riahc')
     end
   end
 end
