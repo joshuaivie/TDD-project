db9d7829202d8bd120258e4d6ec528cb0dd04e7b
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-28 21:53:53 +0300

    Create: test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,9 +2,16 @@ require_relative '../solver.rb'
 
 describe Solver do
   context "check for factorial method" do
+    before :each do
+      @solver=Solver.new
+    end
     it "check for valid result" do
+      expect(@solver.factorial(5)).to eq(120)
+      expect(@solver.factorial(4)).to eq(24)
+      expect(@solver.factorial(0)).to eq(1)
     end
     it "check for Exception" do
+      expect(@solver.factorial(-5)).to raise_error("Integer must be a posetive whole number")
     end
   end
 end
