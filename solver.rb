3c55779dd7b233470e6b5377d66682a1a548bc54
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-07-28 21:04:00 +0100

    add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -15,4 +15,17 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(number)
+    case number
+    when (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    when (number % 3).zero?
+      'fizz'
+    when (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
 end
