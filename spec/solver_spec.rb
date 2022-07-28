ca81555ab4480f65696a5d7ff41e9c5fde533e11
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-28 21:42:07 +0300

    Create: test structure for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../solver.rb'
 
-describe "a specification" do
-  
+describe Solver do
+  context "check for factorial method" do
+    it "check for valid result" do
+    end
+    it "check for Exception" do
+    end
+  end
 end
